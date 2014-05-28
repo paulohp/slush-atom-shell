6f1f7d477198eeb31e671069d707f132ce93c470
Paulo Pires <taralloqueiroiz@gmail.com>
2014-05-28 15:21:42 -0300

    added a template

---
+++ templates/gulpfile.js
@@ -0,0 +1,22 @@
+var gulp  = require('gulp')
+var shell = require('gulp-shell')
+
+// Run project
+gulp.task('run', shell.task([
+
+]))
+
+// Compile project
+gulp.task('osx', shell.task([
+
+]))
+
+// Compile project
+gulp.task('win', shell.task([
+
+]))
+
+// Compile project
+gulp.task('linux', shell.task([
+
+]))
